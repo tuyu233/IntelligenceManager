95e18553a48e65f8119d136ad16a71a0c170a656
zen chen <chenzenian@live.com>
2016-07-07 18:13:50 +0800

    add more db function

--- src/database/DatabaseHelper.java
+++ src/database/DatabaseHelper.java
@@ -1,11 +1,17 @@
 package database;
 
-import java.sql.Date;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Expression;
+import org.hibernate.criterion.Restrictions;
+
 import entity.Record;
 import util.ClassUtil;
 
@@ -145,6 +151,43 @@ public class DatabaseHelper {
 	}
 	
 	/**
+	 * search keyword by year
+	 * @param keyWord
+	 * @param year
+	 * @return
+	 */
+	public static List<Record> search(String keyWord,String year) {
+		Session session = HibernateUtil.getSession();
+		String hql = "from Record a";
+		
+		hql += " where (a.content like '%"
+				+ keyWord + "%' or a.title like '%" + keyWord + "%')";
+		
+		hql += " and (";
+
+		DateFormat  df = new SimpleDateFormat("yyyy-MM-dd");
+		Date start,end;
+		try {
+			start = df.parse(year+"-1-1");
+			end = df.parse((Integer.valueOf(year)+1)+"-1-1");
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			start = new Date(0);
+			end = new Date(Integer.MAX_VALUE);
+			e.printStackTrace();
+		}
+		hql += " a.saveTime >= :start and a.saveTime < :end";
+		hql += ")";
+		hql += "order by a.saveTime desc";
+		Query query = session.createQuery(hql);
+		query.setDate("start", start);
+		query.setDate("end", end);
+		List<Record> list = query.list();
+		session.close();
+		return list;
+	}
+	
+	/**
 	 * count number of record with given conditions.
 	 * @param keyWord
 	 * @param type
@@ -168,10 +211,52 @@ public class DatabaseHelper {
 		Query query = session.createQuery(hql);
 		long count = (long) query.uniqueResult();
 		session.close();
-		//System.err.println(query.uniqueResult()+" "+query.uniqueResult().getClass().getName());
 		return (int) count;
 	}
 	
+	/**
+	 * count keyWord by year
+	 * @param keyWord
+	 * @return
+	 */
+	public static Map<String,Integer> count(String keyWord){
+		Map<String,Integer> map = new HashMap<>();
+		SimpleDateFormat sf = new SimpleDateFormat("YYYY");
+		
+		Session session = HibernateUtil.getSession();
+		String hql = "from Record a";
+		
+		hql += " where (a.content like '%"
+				+ keyWord + "%' or a.title like '%" + keyWord + "%')";
+		
+		Query query = session.createQuery(hql);
+		List<Record> list = query.list();
+		session.close();
+		
+		for(Record record : list){
+			String year = sf.format(record.getSaveTime());
+			if(!map.containsKey(year)){
+				map.put(year, 1);
+			}
+			else {
+				int count = map.get(year);
+				map.put(year, count+1);
+			}
+		}
+		
+		return map;
+	}
+	
+	/**
+	 * get all record
+	 * @return
+	 */
+	public static List<Record> getAllRecord(){
+		Session session = HibernateUtil.getSession();
+		List<Record> list = session.createQuery("from Record a order by a.id asc").list();
+		session.close();
+		return list;
+	}
 	/**
 	 * save object to database
 	 * @param object
@@ -222,4 +307,10 @@ public class DatabaseHelper {
 		session.close();
 	}
 
+	public static void main(String args[]) {
+		Session session = HibernateUtil.getSession();
+		Query query = session.createQuery("select distinct a.author  from Record a");
+		System.out.println(query.list());
+		session.close();
+	}
 }
