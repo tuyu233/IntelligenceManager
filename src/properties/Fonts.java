fdddae5ea7016a43ac6f2ef7309f58f9cc7febff
amisyy <amisyy@sina.cn>
2016-07-07 10:12:18 +0800

    添加字体文件

--- src/properties/Fonts.java
+++ src/properties/Fonts.java
@@ -1,5 +1,20 @@
 package properties;
 
-public class Fonts {
+import java.awt.Font;
 
+public final class Fonts {
+
+	public static Font title = new Font("微软雅黑", Font.ROMAN_BASELINE, 40);
+	
+	public static Font normal = new Font("微软雅黑", Font.PLAIN, 15);
+	
+	public static Font keyword = new Font("微软雅黑", Font.PLAIN, 20);
+	
+	public static Font opinion_title = new Font("微软雅黑", Font.PLAIN, 25);
+	
+	public static Font opinion_index = new Font("微软雅黑", Font.PLAIN, 60);
+	
+	public static Font searchBar = normal;
+
+	public static Font searchButton = normal;
 }
