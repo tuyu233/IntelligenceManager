ddcd584b4f2266af8941a258e86f3ab7e6c55516
amisyy <amisyy@sina.cn>
2016-07-07 13:44:45 +0800

    添加界面
    添加了主界面和三个tab

--- src/properties/Attributes.java
+++ src/properties/Attributes.java
@@ -1,5 +1,20 @@
 package properties;
 
-public class Attributes {
-
+public class Attributes 
+{
+	public static int MAIN_FRAME_WIDTH = 1366;
+	
+	public static final String TAGCLOUD_SHAPEFILE = "./resource/tagcloud/shape.png";
+	
+	public static final String TITLENAME = "舆论监测系统";
+	
+	public static final String TOOLTIP = "请输入你想搜索的关键词";
+	
+	public static final String SEARCHBUTTON = "搜索";
+	
+	public static final String RESULTBUTTON = "显示结果";
+	
+	public static final String SEARCHRESULTTAB = "搜索结果";
+	
+	public static final String WHOLEPASSAGE = "显示全文";
 }
