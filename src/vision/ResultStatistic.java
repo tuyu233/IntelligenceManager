0907bc5dfebd4c6f1d47cefe3a2d94e6fa27f6e6
amisyy <amisyy@sina.cn>
2016-07-16 21:59:20 +0800

    修改panel2布局

--- src/vision/ResultStatistic.java
+++ src/vision/ResultStatistic.java
@@ -7,6 +7,7 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.Panel;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
@@ -132,8 +133,7 @@ public class ResultStatistic extends JPanel
 	private void tagcloud()
 	{
 		//TODO
-		String str=Util.tagCloudTrans(keywords.get(0));//全部的关键词
-				//"神话中,25\n孩子,25\n柠檬水,25\n压缩,25\n美国,25\n柠檬,25\n就出,25\n黑暗中,25\n大脑,25\n小狗,24白色,18\n小部件,18\n企鹅,18\n薄荷,18\n低,18\n屋顶,17\n合资企业,17\n最爱,17\n鼻子,17\n太阳,17\n客户,17\n狗,17\n海洋,16\n处理,16\n粉色,16\n发现,15\n风险,15";
+		String str="神话中,25\n孩子,25\n柠檬水,25\n压缩,25\n美国,25\n柠檬,25\n就出,25\n黑暗中,25\n大脑,25\n小狗,24\n白色,18\n小部件,18\n企鹅,18\n薄荷,18\n低,18\n屋顶,17\n合资企业,17\n最爱,17\n鼻子,17\n太阳,17\n客户,17\n狗,17\n海洋,16\n处理,16\n粉色,16\n发现,15\n风险,15";
 		TagCloudHelper.getInstance().makeTagcloud(str,"./output/tagcloud.png");
 		try
 		{
@@ -149,6 +149,7 @@ public class ResultStatistic extends JPanel
 		tagCloud.init();
 		panel[7].add(tagCloud,BorderLayout.CENTER);
 		tagCloud.stop();
+		System.out.print(str);
 	}
 	
 	private void module2Setup()
@@ -169,22 +170,25 @@ public class ResultStatistic extends JPanel
 		note[2].setBorder(new EtchedBorder());
 		
 		//最左边的panel
-		gbc2.weightx=0.13;
+		gbc2.weightx=0.5;
 		gbc2.gridheight=4;
 		gbc2.gridwidth=8;
 		addComponent2(panel[1]);
 		
 		//第五行
+		gbc2.weightx=0;
 		gbc2.gridheight=1;		
 		gbc2.gridwidth=8;
 		addComponent2(label[8]);
 		label[8].setBackground(Color.WHITE);
 		label[8].setOpaque(true);
 		label[8].setText(Attributes.WHOLEWEB);
+		gbc2.weightx=1;
 		gbc2.gridwidth = GridBagConstraints.REMAINDER;
 		addComponent2(panel[0]);
 		
 		//第六行
+		gbc2.weightx=0;
 		gbc2.gridheight=3;
 		gbc2.gridwidth=8;
 		addComponent2(panel[7]);
@@ -192,11 +196,13 @@ public class ResultStatistic extends JPanel
 		tagcloud();
 		
 		//右三行
+		gbc2.weightx=0.5;
 		gbc2.gridheight=3;
 		gbc2.gridwidth=1;
 		addComponent2(panel[8]);
 		
 		//政府
+		gbc2.weightx=1;
 		gbc2.weightx=0;
 		gbc2.gridheight=1;
 		gbc2.gridwidth=1;
@@ -303,8 +309,6 @@ public class ResultStatistic extends JPanel
 			else
 				label[i].setFont(Fonts.opinion_title);
 		}
-			
-		
 	}
 
 }
